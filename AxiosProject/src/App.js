4802065390739e240bc793098b83b84315d88a24
bella <bellakongqn@gmail.com>
2020-04-04 22:03:14 +0800

    prox

--- AxiosProject/src/App.js
+++ AxiosProject/src/App.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {get, post} from './http'
 
 export default function App(){
-    const params = {id:1,name:'hha'}
-    const getData =() =>{
-        get('/',params)
+    const [list, setList] = useState([])
+    const [msg, setMsg] = useState(null)
+    const params = {
+        id: 1,
+        name:'haha'
+    }
+    const getData = async () =>{
+        const response = await get('/api/names',params)
+        setList(response.data)
     }
 
-    const postData =() =>{
-        post('/',params)
+    const postData = async () => {
+        const response = await post('/api/update',params)
+        setMsg(response.msg)
     }
 
     return(
         <div>
             <button onClick={getData}> get </button>
+            {
+                list.map((item,index)=>(
+                    <li key={index}>{item}</li>
+                ))
+            }
             <br/>
             <button onClick={postData}>post</button>
+            {msg}
         </div>
     )
 }
