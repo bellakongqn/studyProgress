0c1e7ed557a1bf5fb7aefaafcf1447c91b81a29a
bella <bellakongqn@gmail.com>
2020-04-06 21:55:59 +0800

    http

--- ts-app/src/App.tsx
+++ ts-app/src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {get, post} from './http'
 
 function App() {
+
+  const [list, setList] = useState<Array<string>>([])
+  const [msg, setMsg] = useState<string>('')
+  const params = {
+      id: 1,
+      name:'haha'
+  }
+  const getData = async () =>{
+      const response = await get('/names',params)
+      setList(response.data)
+  }
+
+  const postData = async () => {
+      const response = await post('/update',params)
+      setMsg(response.data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={getData}> get </button>
+      {
+          list.map((item,index)=>(
+              <li key={index}>{item}</li>
+          ))
+      }
+      <br/>
+      <button onClick={postData}>post</button>
+      {msg}
     </div>
   );
 }
---
+++ ts-app/src/http.tsx
@@ -0,0 +1,62 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+// 环境的切换
+if (process.env.NODE_ENV === 'development') {
+    axios.defaults.baseURL = '/api';
+}
+// 超时取消请求
+axios.defaults.timeout = 10000;
+// post 请求头
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+// 请求拦截
+axios.interceptors.request.use( (config: AxiosRequestConfig) => {
+    // 一般在这个位置判断token是否存在 config.headers.Authorization = token
+    // getToken()
+    return config;
+}, (error: any) => {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+});
+
+// 响应拦截
+axios.interceptors.response.use(
+    (response: AxiosResponse<any>) => {
+        // 处理响应数据
+        if (response.status === 200) {
+            return Promise.resolve(response.data);
+        } else {
+            return Promise.reject(response);
+        }
+    }, 
+    (error) => {
+        // 处理响应失败
+        return Promise.reject(error)
+    }
+);
+
+// get
+export function get(url:string, params:any):Promise <AxiosResponse>{
+    return axios.get(url, {
+        params: params
+    })
+}
+
+// post
+export function post(url:string, params:any):Promise <AxiosResponse> { 
+    return axios.post(url, params)
+}
+
+// put
+export function put(url:string, params:any):Promise <AxiosResponse>{
+    return axios.put(url, params )
+}
+
+// delete
+export function deleteFc(url:string, params:any):Promise <AxiosResponse>{
+    return axios.delete(url, params)
+}
+
+/*
+export const gettt = <T = {}>(url:string, config?: AxiosRequestConfig) => axios.get<AxiosResponse <T>>(url, config)
+*/
